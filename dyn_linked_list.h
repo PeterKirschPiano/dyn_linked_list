8d1d80be384f6deb8a31080397ea8572c0cfede6
PeterKirschPiano <153505913+PeterKirschPiano@users.noreply.github.com>
2025-05-20 15:27:43 +0200

    Update dyn_linked_list.h

--- dyn_linked_list.h
+++ dyn_linked_list.h
@@ -13,13 +13,12 @@ typedef struct
     node_t  *head_p,
             *rear_p;
 
-    size_t  key_size; //number of bytes of 1 element
     int     n_elements;
 }list_t;
 
 //****MEMORY MANAGMENT****
 //initialize the list
-void    list_initialize(list_t *list, size_t key_size);
+void    list_initialize(list_t *list);
 //allocate memory for a node and initialize it
 node_t  *list_node_create(void *new_key, node_t *previous_node_p, node_t *next_node_p);
 //free the memory of a node and update the list
@@ -29,9 +28,9 @@ int     list_node_destroy(node_t *node, list_t *list);
 //add an element to the rear of the list
 int     list_element_add(void *new_key, list_t *list);
 //return memory adress the key you are looking for
-void *list_element_find(void *key, list_t *list_p);
+void    *list_element_find(void *key, size_t key_size, list_t *list_p);
 //return a complete node pointer by searching for its key
-node_t *list_element_find_node(void *key, list_t *list_p); // -> maybe change to void* return to key
+node_t  *list_element_find_node(void *key, size_t key_size, list_t *list_p); // -> maybe change to void* return to key
 
 //****QUEUE****
 //remove the head, return the value as a pointer and update list
